254b571fa9ea1ef36eb82643f15ae2a6a2933008
reetou <fillmasterson@gmail.com>
2025-11-01 14:05:43 +0100

    Fix build err

--- src/app/auth/connect/[id]/route.ts
+++ src/app/auth/connect/[id]/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server'
 // The client you created from the Server-Side Auth instructions
 import { createServerClient } from '@/supabase/server'
 
-export async function GET(request: Request, { params }: { params: { id: string } }) {
+export async function GET(request: Request, { params }: { params: Promise<{ id: string }> }) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
   // if "next" is in param, use it as the redirect URL
